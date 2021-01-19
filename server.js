474505c87f3cb0d1e7d2be5178921fe88fe1ad34
michelss2018 <michellss2018@gmail.com>
2021-01-19 15:27:31 -0300

    Criando e configurando um servidor

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+server.get("/", function(req, res){
+    return res.sendFile(__dirname + "/index.html")
+})
+
+server.get("/ideias", function(req, res){
+    return res.sendFile(__dirname + "/ideias.html")
+})
+
+server.listen(3500)
